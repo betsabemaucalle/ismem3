793a5ab6301d7d9dd4c39a27955a328cc6671d54
Betsabe Maucalle <maucalleb@gmail.com>
2018-10-16 22:11:55 -0500

    Agregue opción de Foto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,55 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	public base64Image : string;
+
+  constructor(public navCtrl: NavController, private camera : Camera) {
+
+  }
+
+  tomarFoto() {
+
+  	const options : CameraOptions = {
+  		destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+  	}
+  	this.camera.getPicture(options).then((imageData) => {
+	 	this.base64Image = 'data:image/jpeg;base64,' + imageData;
+	}, (err) => {
+		console.log(err)
+	});
+
+  }
+
+  import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+	public base64Image : string;
+
+  constructor(public navCtrl: NavController, private camera : Camera) {
+
+  }
+
+  tomarFoto() {
+
+  	const options : CameraOptions = {
+  		destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+  	}
+  	this.camera.getPicture(options).then((imageData) => {
+	 	this.base64Image = 'data:image/jpeg;base64,' + imageData;
+	}, (err) => {
+		console.log(err)
+	});
 
   }
 
